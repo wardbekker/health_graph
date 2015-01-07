3997b60da786c5387069adc59dae169c22af03fe
Kerstin Knebel <kerstin.knebel@gmail.com>
2015-01-07 10:14:41 +0100

    added diabetes measurements

---
+++ lib/health_graph/models/diabetes_measurement_feed.rb
@@ -0,0 +1,24 @@
+module HealthGraph
+  class DiabetesMeasurementFeed
+    include Model              
+    
+    hash_attr_accessor :items, :next
+    
+    class Item 
+      include Model      
+      
+      hash_attr_accessor :timestamp, :fasting_plasma_glucose_test, :oral_glucose_tolerance_test, :random_plasma_glucose_test
+      
+      def initialize(hash) 
+        populate_from_hash! hash
+      end      
+    end
+                      
+    def initialize(access_token, path)            
+      self.access_token = access_token
+      response = get path, HealthGraph.accept_headers[:diabetes_measurement_feed]
+      self.body = response.body
+      populate_from_hash! self.body                  
+    end                           
+  end
+end
