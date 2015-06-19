db3b957e81d19070f6432f0900f284310f74f0ac
Axel Manzano <axel@goodgym.org>
2015-06-19 10:59:56 +0100

    typo merge

--- lib/health_graph/configuration.rb
+++ lib/health_graph/configuration.rb
@@ -41,7 +41,7 @@ module HealthGraph
       records: 'application/vnd.com.runkeeper.Records+json',
       profile: 'application/vnd.com.runkeeper.Profile+json',
       settings: 'application/vnd.com.runkeeper.Settings+json',
-      change_log: => "application/vnd.com.runkeeper.ChangeLog+json",
+      change_log: "application/vnd.com.runkeeper.ChangeLog+json",
       root: "application/vnd.com.runkeeper.Root+json"
     }.freeze
 
